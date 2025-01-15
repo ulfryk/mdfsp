62e312cd331201e16006fffb683fe9e1c5c92dbc
Jakub Strojewski <jakub.strojewski@gmail.com>
2025-01-15 12:16:56 +0000

    refactor: remove ambiguous stream monetization problem

--- src/main/scala/distribution/domain/Stream.scala
+++ src/main/scala/distribution/domain/Stream.scala
@@ -4,10 +4,12 @@ import java.time.Instant
 import scala.concurrent.duration.FiniteDuration
 
 opaque type StreamId = Long
+opaque type StreamSequenceId = Long
 
 // TODO: So our ubiquitous language clashes with common abstractions…
 case class Stream(
   id: StreamId,
+  sequenceId: StreamSequenceId,
   songId: SongId,
   duration: FiniteDuration, // or should we use our own type on top of Int?
   startedAt: Instant,
